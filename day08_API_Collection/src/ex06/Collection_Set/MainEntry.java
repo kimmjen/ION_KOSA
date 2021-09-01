0ff97e705c40a6b238713399c4f76ab2a3273aab
kimmjen <50638199+kimmjen@users.noreply.github.com>
2021-09-01 17:06:57 +0900

    day08_api_collection

---
+++ day08_API_Collection/src/ex06/Collection_Set/MainEntry.java
@@ -0,0 +1,7 @@
+package ex06.Collection_Set;
+
+public class MainEntry {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ week2/src/p_2021_08_31/Employee.java
@@ -0,0 +1,5 @@
+package p_2021_08_31;
+
+public abstract class Employee {
+
+}
