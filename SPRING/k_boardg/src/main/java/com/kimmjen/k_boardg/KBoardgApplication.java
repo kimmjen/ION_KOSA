910ab2031f89b5aa1f2329ffd2780b55f3778d02
kimmjen <wpals814@gmail.com>
2021-09-18 23:14:40 +0900

    알마인드 교체

---
+++ SPRING/k_boardg/src/main/java/com/kimmjen/k_boardg/KBoardgApplication.java
@@ -0,0 +1,13 @@
+package com.kimmjen.k_boardg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KBoardgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KBoardgApplication.class, args);
+	}
+
+}
---
+++ SPRING/k_boardg/src/test/java/com/kimmjen/k_boardg/KBoardgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimmjen.k_boardg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KBoardgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
